9b5918440e3aea039cea07a8b4a72a5d6c5d740e
Matthew Hellmer <theprankmonkey@gmail.com>
2022-07-06 17:02:03 -0400

    Linted.

--- src/__init__.py
+++ src/__init__.py
@@ -28,9 +28,11 @@ def create_app(script_info=None):
 
     # register blueprints
     from src.api.ping import ping_blueprint
+
     app.register_blueprint(ping_blueprint)
 
     from src.api.users.views import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     # shell context for flask cli
